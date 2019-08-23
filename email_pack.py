d178ed645957e1939834f8d9fe8130868b9dac5a
XinyaoTian <leontian1024@gmail.com>
2019-08-23 10:56:33 +0800

    token creating & receiving (without email password in config.py)

--- email_pack.py
+++ email_pack.py
@@ -3,6 +3,7 @@ from flask import render_template
 from app import mail
 from app import app
 from threading import Thread
+from config import Config
 
 
 # send async email
@@ -23,19 +24,21 @@ def send_email(subject, sender, recipients, text_body, html_body):
 # send register confirm email
 def send_register_email(account_dict):
     token = account_dict['token']
+    register_url = 'http://' + Config.AUTH_UI_REGISTER_CONFIRM_URL + token
     send_email('Registration Verification',
                sender=app.config['ADMINS'][0],
                recipients=[account_dict['account_email']],
-               text_body=render_template('email/register_template.txt', token=token),
-               html_body=render_template('email/register_template.html', token=token))
+               text_body=render_template('email/register_template.txt', register_url=register_url),
+               html_body=render_template('email/register_template.html', register_url=register_url))
 
 
 # send password resetting email
 def send_password_reset_email(account_dict):
     token = account_dict['token']
+    reset_url = 'http://' + Config.AUTH_UI_RESET_PASSWORD_URL + str(token)
     send_email('Reset Password',
                sender=app.config['ADMINS'][0],
                recipients=[account_dict['account_email']],
-               text_body=render_template('email/reset_password.txt', token=token),
-               html_body=render_template('email/reset_password.html', token=token))
+               text_body=render_template('email/reset_password.txt', reset_url=reset_url),
+               html_body=render_template('email/reset_password.html', reset_url=reset_url))
 
